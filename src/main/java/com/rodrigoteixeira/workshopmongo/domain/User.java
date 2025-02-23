8d431907c2659c49212baff2d1e5229511148ec5
Rodrigo <rtrs.eu@gmail.com>
2025-02-22 21:00:24 -0300

    feat: Referenciando os posts do usuário

--- src/main/java/com/rodrigoteixeira/workshopmongo/domain/User.java
+++ src/main/java/com/rodrigoteixeira/workshopmongo/domain/User.java
@@ -1,28 +1,43 @@
 package com.rodrigoteixeira.workshopmongo.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Document
-@Data
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Getter @Setter
     @Id
     private String id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private String email;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Getter @Setter
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
